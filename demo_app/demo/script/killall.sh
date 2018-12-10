4bd9fe39b602a925fa65de06663e001e8fc8ab1f
guoff <guocdfeifei@163.com>
2018-12-10 15:34:29 +0800

    图片优化

---
+++ demo_app/demo/script/killall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+NAME="demo_app"
+if [ ! -n "$NAME" ];then
+    echo "no arguments"
+    exit;
+fi
+
+echo $NAME
+ID=`ps -ef | grep "$NAME" | grep -v "$0" | grep -v "grep" | awk '{print $2}'`
+echo $ID
+echo "################################################"
+for id in $ID
+do
+kill -9 $id
+echo "kill $id"
+done
+echo  "################################################"
+#uwsgi --ini /www/web/sub_manager/script/uwsgi.ini
+#chmod 777 /www/web/sub_manager/script/uwsgi.sock
